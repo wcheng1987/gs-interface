28986b46466e13be50f3ba909fd8906eb6b61087
guanbo <guanbo2002@gmail.com>
2012-06-25 18:18:06 +0800

    location:coutrywide fid =1

--- location.js
+++ location.js
@@ -4,6 +4,8 @@ exports.index = function(req, res) {
     var sql = 'SELECT * FROM `gs_location`';
     db.query(sql, function(err, rs) {
         if(db.errorHandle(err, rs, function(result) {res.send(result.status);})) {
+            rs[0].fid = 1;
+//            console.log(rs[0]);
             var locations = {location: rs};
             res.json(locations);
         }    
