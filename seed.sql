c1d1c68aa1b810a673acf2d20615cd974910bc88
guanbo <guanbo2002@gmail.com>
2012-11-01 00:33:05 +0800

    Revert "整理sql脚本"
    This reverts commit 318d702f97de97042cd622829aee23cfab06d7b8.

--- seed.sql
+++ seed.sql
@@ -12,10 +12,6 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-
-DROP DATABASE IF EXISTS `goldstone`;
-CREATE DATABASE `goldstone`;
-
 --
 -- Table structure for table `admin`
 --
