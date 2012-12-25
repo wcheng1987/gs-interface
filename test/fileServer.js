5bbf9220aaa9ca757b635478fb04a706f04dc08d
guanbo <guanbo2002@gmail.com>
2012-12-25 16:01:41 +0800

    修复下载文件错误

--- test/fileServer.js
+++ test/fileServer.js
@@ -1,19 +1,21 @@
+var config = require('config.js').config;
 var Member = require('./support/member');
 var file = require('./support/http.file');
-var config = require('config.js').config;
+var files = require('./fixtures/files.json');
 
 var member = new Member();
-var objectURL = 'http://'+config.host+':1339/files/audio/fat.mp3';
-// var objectURL = 'http://'+config.host+':1339/files/audio/spend a lot of time with sb..mp3';
 describe('File Server Unit Test', function(){
 	before(function(done) {
 	  member.auth(done)
 	})
 	describe('#GET binary data', function() {
-		it('should success get '+objectURL, function(done) {
-			file.get(member.session.sid, objectURL, function() {
-				done();
-			});
+		files.locals.forEach(function(filename) {
+			var objectURL = 'http://'+config.host+':1339/files/audio/'+filename;
+			it('should success get '+objectURL, function(done) {
+				file.get(member.session.sid, objectURL, function() {
+					done();
+				});
+			})
 		})
 	})
 })
