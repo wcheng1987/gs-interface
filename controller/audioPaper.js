b0b0e93048c4a49ac82ce8e66e88e6e0cd1da87c
guanbo <guanbo2002@gmail.com>
2013-01-18 18:01:58 +0800

    新增听力试卷动态返回204情况

--- controller/audioPaper.js
+++ controller/audioPaper.js
@@ -2,13 +2,20 @@ var config = require('config.js').config;
 var logger = require('log4js').getLogger('AudioPaper Controller')
 var model = require('../model')
 var AudioPaper = model.AudioPaper
+var util = require('../lib/util')
+var redis = require('redis')
+var redisClient = redis.createClient(config.redis.port, config.redis.host)
+
+var kvCache = {
+	size:100,
+	maxAge:60
+}
 
 exports.publicTimeLine = function(req, res, next) {
-	var timeline = req.headers['if-modified-since']|| '1970-1-1 12:12:12';
+	var now = util.getNow()
+	var timeline = req.headers['if-modified-since']||now
 	var opt = {
-		query: {
-			createtime: {'>': "'"+timeline+"'"},
-		},
+		query: {},
 		order: {
 			createtime: 'DESC'
 		},
@@ -16,16 +23,78 @@ exports.publicTimeLine = function(req, res, next) {
 			start: req.query.start || 1,
 			end: req.query.end || 10
 		}
-	};
+	}
+	logger.debug('AudioPaper PublicTimeLine Last-Modified', timeline)
+	var start = opt.limit.start-1
+	, stop = parseInt(opt.limit.end)
+	, key = 'AudioPaperPublicTimeLine'+timeline
 	
-	var ap = new AudioPaper()
-	
-	ap.find(opt, function(err, aps) {
-    if(err) return next(err);
+	var send = function(apArr){
+		var aps = apArr.slice(start, stop)
 		if(aps.length > 0) {
-			return res.json({audioPaper:aps}, {'Last-Modified': new Date()})
+			return res.json({audioPaper:aps}, {'last-modified': timeline})
 		} else {
-			return res.send(304)
+			if(start === 0) {
+				return res.send(304)
+			} else {
+				return res.send(204)				
+			}
 		}
-	})
+	}
+
+	var cache = function(aps) {
+		/*
+		var args = [key].concat(aps.map(function(ap){
+			return JSON.stringify(ap)
+		}))
+		args.push(function(err, reply){
+			if(err) return next(err)
+			redisClient.expire(key, kvCache.maxAge, function(err, reply){
+				if(err) return next(err)
+				send(aps.slice(start, stop))
+			})
+		})
+		redisClient.rpush.apply(redisClient, args)*/
+		redisClient.multi()
+		.set(key, JSON.stringify(aps))
+		.expire(key, kvCache.maxAge)
+		.exec(function(err, replies){
+			if(err) return next(err)
+			send(aps)
+		})
+	}
+
+	var query = function(){
+		var ap = new AudioPaper()
+		ap.find(opt, function(err, aps) {
+	    if(err) return next(err)
+			if(opt.limit.start === 1 && opt.limit.end === kvCache.size) {
+				cache(aps)
+			} else {
+				send(aps);
+			}
+		})
+	}
+
+	logger.trace('gbo===', start, stop, kvCache, now)
+	if(stop > kvCache.size) {
+		query()
+	} else {
+		redisClient.ttl(key, function(err, reply){
+			if(err) return next(err)
+			if(reply === -1) {
+				opt.limit.start = 1
+				opt.limit.end = kvCache.size
+				query()
+			} else {
+				if(start === 0) return send([]);
+				redisClient.get(key, function(err, reply){
+					if(err) return err
+					var aps = JSON.parse(reply)
+					// logger.trace(aps)
+					send(aps)
+				})
+			}
+		})
+	}
 };
--- test/acceptance/audioPaper.js
+++ test/acceptance/audioPaper.js
@@ -1,25 +1,50 @@
 var request = require('../support/http');
 var util = require('../../lib/util');
 
+var url = '/audio_paper/public_timeline'
+
 describe('AudioPaper public time line', function(){
-  it('should success get last 10', function(done){
+	var lastModified = null;
+	before(function(done){
     request()
-		.get('/audio_paper/public_timeline')
+		.get(url)
 		.end(function(res) {
 			res.statusCode.should.equal(200)
 			res.should.be.json
 			res.body.should.have.property('audioPaper')
 			res.body.audioPaper.length.should.be.above(0)
+			res.headers.should.have.property('last-modified')
+			console.log('Last-Modified',res.headers['last-modified'])
+			lastModified = res.headers['last-modified']
 			done()
+	  })
+	})
+	describe('#After succeed got Audio Paper public time line', function(){
+		describe('##Then refresh', function(){
+		  it('should get already update', function(done){
+		    request()
+				.get(url)
+				.set('if-modified-since', lastModified)
+				.expect(304, done)
+		  })
 		})
-  })
-  it('should not get data', function(done){
-    request()
-		.get('/audio_paper/public_timeline')
-		.set('if-modified-since', util.addMonthFromNow(0))
-		.end(function(res) {
-			res.statusCode.should.equal(304)
-			done()
+		describe('##Then got more data ', function(){
+			it('should get next 10 data', function(done){
+		    request()
+				.get(url+'?start=11&end=20')
+				.set('if-modified-since', lastModified)
+				.expect(200, done)
+				// .end(function(res){
+				// 	console.log(res.body.audioPaper[0])
+				// 	done()
+				// })
+			})
+		  it('should get nothing when out of scope', function(done){
+		    request()
+				.get(url+'?start=9999901&end=9999910')
+				.set('if-modified-since', lastModified)
+				.expect(204, done)
+		  })
 		})
-  })
+	})
 })
