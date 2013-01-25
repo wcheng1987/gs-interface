07a7cfb3e95082db3579d668c9e13b43351252b7
guanbo <guanbo2002@gmail.com>
2013-01-25 15:01:11 +0800

    新增听力教材按父目录查询的方式

--- controller/bookCatalog.js
+++ controller/bookCatalog.js
@@ -7,6 +7,7 @@ exports.get = function(req, res, next) {
 	var opt = {query:{}}
 	if(req.query.name) opt.query.name = req.query.name
 	if(req.query.level) opt.query.level = req.query.level
+	if(req.query.parent_id) opt.query.parent_id = parseInt(req.query.parent_id)
 	opt.limit = {
 		start:req.query.start||1,
 		end:req.query.end||30
--- test/acceptance/bookCatalog.js
+++ test/acceptance/bookCatalog.js
@@ -2,11 +2,13 @@ var request = require('../support/http');
 
 var url = '/book_catalogs'
 var catalog = "初中"
+var parentID = 1
+var level = 0
 
 describe('Book Catalog', function(){
-  it('should success get 30 top level catalog', function(done){
+  it.only('should success get 30 catalog which level: '+level, function(done){
     request()
-		.get(url+'?level=0')
+		.get(url+'?level='+level)
 		.end(function(res) {
 			res.statusCode.should.equal(200)
 			res.should.be.json
@@ -27,4 +29,16 @@ describe('Book Catalog', function(){
 			done()
 		})
   })
+	it('should success get catalog which parent id:'+parentID, function(done){
+		request()
+		.get(url+'?parent_id='+parentID)
+		.end(function(res) {
+			res.statusCode.should.equal(200)
+			res.should.be.json
+			res.body.should.have.property('bookCatalog')
+			res.body.bookCatalog.length.should.be.above(0)
+			res.body.bookCatalog[0].should.have.property('parent_id', ''+parentID)
+			done()
+		})
+	})
 })
